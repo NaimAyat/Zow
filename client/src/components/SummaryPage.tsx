fef9e56dab478e12f6cf6cb448f235f95c5571c5
Christopher Aziz <caziz@ucla.edu>
2018-11-07 20:36:15 -0800

    Create simple shell pages

---
+++ client/src/components/SummaryPage.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Header } from "semantic-ui-react";
+
+class SummaryPage extends React.Component {
+  public render() {
+    return (
+      <Header as="h1" textAlign="center">
+        Summary View
+      </Header>
+    );
+  }
+}
+
+export default SummaryPage;
