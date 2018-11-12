a8c642e790a0b5ce583fb5654801a1ebf7fd7994
Dmitri Brereton <mitrikyle@gmail.com>
2018-11-11 18:04:09 -0800

    we know it's a password hash

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema
 let UserSchema : Schema = new Schema({
   name: {type: String},
   email: {type: String, required: true},
-  passwordHash: {type: String, required: true}
+  password: {type: String, required: true}
 })
 
 const User = mongoose.model('User', UserSchema)
