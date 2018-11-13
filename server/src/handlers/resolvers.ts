9e1c1dddaf1e27dda208d28e442ae38b9f3763f5
Austin Berke <austinberke@gmail.com>
2018-11-13 11:08:17 -0800

    Added resolvers for user service

--- server/src/handlers/resolvers.ts
+++ server/src/handlers/resolvers.ts
@@ -3,15 +3,36 @@ import { IAuthorizationService } from "../services/auth";
 export default function getResolvers(authService: IAuthorizationService) {
   return {
     Query: {
-      async currentUser(parent, args, ctx) {
+      // Authentication
+      async currentUser(parent, args, ctx): Promise<IUser> {
         console.log(ctx.session);
         return ctx.session.user;
-      }
+      },
+      // Form Service
+      async form(parent, args, ctx) {
+        const { formID } = args;
+        return await formService.getFormsByID(ctx, formID);
+      },
+      async forms(parent, args, ctx) {
+        const { user } = args;
+        return await formService.getFormsByUser(ctx, user);
+      },
+      async questions(parent, args, ctx) {
+        const { formID } = args;
+        return await formService.getQuestions(ctx, formID);
+      },
+      async responses(parent, args, ctx) {
+        const { formID } = args;
+        return await formService.getResponses(ctx, formID);
+      },
+
+
     },
     Mutation: {
+      // Authentication Service
       async login(parent, args, ctx): Promise<boolean> {
         const { email, password } = args;
-        return await authService.login(ctx, args.email, args.password);
+        return await authService.login(ctx, email, password);
       },
       async newUser(parent, args, ctx): Promise<boolean> {
         const { name, email, password } = args;
@@ -20,6 +41,18 @@ export default function getResolvers(authService: IAuthorizationService) {
       async logout(parent, args, ctx): Promise<boolean> {
         await authService.logout(ctx);
         return true;
+      },
+      // User Service
+      async createForm(parent, args, ctx) {
+        return await formService.createNewForm(ctx.session.user);
+      },
+      async addResponse(parent, args, ctx) {
+        const { formID, response } = args;
+        return await formService.addResponse(ctx, formID, response);
+      },
+      async addOwner(parent, args, ctx) {
+        const { formID, userID } = args;
+        return await formService.addOwner(ctx, formID, userID);
       }
     }
   };
--- server/src/services/form.ts
+++ server/src/services/form.ts
@@ -1,9 +1,14 @@
-import { IForm, IResponse, IUser } from "../entities";
+import { IForm, IResponse, IUser, IQuestion } from "../entities";
 
 export interface IFormService {
-  createNewForm(author: IUser): Promise<IForm>;
+  // Accessors
   getFormByID(id: string): Promise<IForm>;
+  getFormsByUser(user: IUser): Promise<[IForm]>;
+  getQuestions(formID: string): Promise<[IQuestion]>;
+  getResponses(formID: string): Promise<[IResponse]>;
+  //Mutators
   saveForm(form: IForm): Promise<void>;
+  createNewForm(author: IUser): Promise<IForm>;
   addResponse(formID: string, response: IResponse): Promise<void>;
   addOwner(formID: string, newOwner: IUser): Promise<void>;
 }
