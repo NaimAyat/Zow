61a274ce0beddbbf0d960be375977d33f57d6afd
Christopher Aziz <caziz@ucla.edu>
2018-11-14 20:41:33 -0800

    Add blank pages

---
+++ client/src/components/FormViewPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Header } from "semantic-ui-react";
+
+class FormViewPage extends React.Component {
+  public render() {
+    return <Header as="h3">View Form</Header>;
+  }
+}
+
+export default FormViewPage;
---
+++ client/src/components/ScoringPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Header } from "semantic-ui-react";
+
+class ScoringPage extends React.Component {
+  public render() {
+    return <Header as="h3">Scoring Page</Header>;
+  }
+}
+
+export default ScoringPage;
