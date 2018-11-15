9904b65c2ffcf9b730919e76a0cc5df82fd71543
Christopher Aziz <caziz@ucla.edu>
2018-11-14 20:13:22 -0800

    Add Form Creation Page

--- client/src/components/FormCreationPage.tsx
+++ client/src/components/FormCreationPage.tsx
@@ -1,64 +1,132 @@
 import * as React from "react";
-// @ts-ignore
-import Reorder, { reorder } from "react-reorder";
-import { Form } from "semantic-ui-react";
-import Question from "./Question";
+import { Button, Form, Header, Input, Segment } from "semantic-ui-react";
+import { IQuestion, QuestionType } from "src/DataTypes";
+import QuestionField from "./QuestionField";
 
-const q1 = {
-  prompt: "Whats ur name?",
-  type: "normal type"
-};
-const q2 = {
-  prompt: "How much r u?",
-  type: "normal type"
-};
-const q3 = {
-  prompt:
-    " Sldjfalksdjf lkasj dflkaj sdlkfj alksdjf laksdj flka jldjfalksdjf lkasj dflkaj sdlkfj alksdjf laksdj flka jldksf alksdf jlkasd fjlkaj ldjfalksdjf lkasj dflkaj sdlkfj alksdjf laksdj flka jldksf alksdf jlkasd fjlkaj ldjfalksdjf lkasj dflkaj sdlkfj alksdjf laksdj flka jldksf alksdf jlkasd fjlkaj ldjfalksdjf lkasj dflkaj sdlkfj alksdjf laksdj flka jldksf alksdf jlkasd fjlkaj ldksf alksdf jlkasd fjlkaj sdlkfj alksdjf lajsdlf j",
-  type: "normal type"
-};
-
-const question1 = <Question question={q1} />;
-const question2 = <Question question={q2} />;
-const question3 = <Question question={q3} />;
-
-interface IFormCreationState {
-  questions: JSX.Element[];
+interface IState {
+  questions: IQuestion[];
+  formName: string;
 }
 
-class FormCreationPage extends React.Component<{}, IFormCreationState> {
+class FormCreationPage extends React.Component<{}, IState> {
   public constructor(props: any) {
     super(props);
-    this.state = {
-      questions: [question1, question2, question3].map((element, index) => (
-        <div key={index}>{element}</div>
-      ))
+    const emailQuestion: IQuestion = {
+      prompt: "Email",
+      type: "email"
     };
-    this.onReorder = this.onReorder.bind(this);
+    this.state = { questions: [emailQuestion], formName: "" };
+    this.onChangeFormName = this.onChangeFormName.bind(this);
   }
-  public onReorder(
-    event: any,
-    previousIndex: any,
-    nextIndex: any,
-    fromId: any,
-    toId: any
-  ) {
-    this.setState({
-      questions: reorder(this.state.questions, previousIndex, nextIndex)
-    });
+
+  public getSetQuestion(index: number) {
+    return (question: IQuestion) => {
+      const questions = [...this.state.questions];
+      questions[index] = question;
+      this.setState({ questions });
+    };
+  }
+
+  public getDeleteQuestion(index: number) {
+    return () => {
+      const questions = [...this.state.questions];
+      questions.splice(index, 1);
+      this.setState({ questions });
+    };
+  }
+
+  public getAddQuestion(type: QuestionType) {
+    return () => {
+      const questions = [...this.state.questions];
+      const question: IQuestion = {
+        prompt: "",
+        type
+      };
+      if (type === "checkBox" || type === "radio" || type === "dropDown") {
+        question.options = [];
+      }
+      questions.push(question);
+      this.setState({ questions });
+    };
+  }
+
+  public hasPhoneQuestion(): boolean {
+    for (const question of this.state.questions) {
+      if (question.type === "phone") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public onChangeFormName(event: React.ChangeEvent<HTMLInputElement>) {
+    const formName = event.target.value;
+    this.setState({ formName });
   }
 
   public render() {
     return (
-      <Form size="big">
-        <Reorder
-          onReorder={this.onReorder}
-          reorderId="my-list"
-          lock="horizontal"
-        >
-          {this.state.questions}
-        </Reorder>
-      </Form>
+      <React.Fragment>
+        <Header as="h3">Create Form</Header>
+        <Input
+          fluid
+          size="huge"
+          placeholder="Type form name here..."
+          onChange={this.onChangeFormName}
+        />
+        <Form size="big">
+          {this.state.questions.map((question, index) => (
+            <QuestionField
+              key={index}
+              question={question}
+              setQuestion={this.getSetQuestion(index)}
+              deleteQuestion={this.getDeleteQuestion(index)}
+            />
+          ))}
+        </Form>
+        <Segment textAlign="center">
+          <Header as="h3" content="Add New Question" />
+          {this.hasPhoneQuestion() || (
+            <Button
+              icon="phone"
+              onClick={this.getAddQuestion("phone")}
+              content="Phone"
+            />
+          )}
+          <Button
+            icon="window minimize outline"
+            onClick={this.getAddQuestion("shortText")}
+            content="Short Text"
+          />
+          <Button
+            icon="bars"
+            onClick={this.getAddQuestion("longText")}
+            content="Long Text"
+          />
+          <Button
+            icon="dot circle"
+            onClick={this.getAddQuestion("radio")}
+            content="Radio Button"
+          />
+          <Button
+            icon="check square"
+            onClick={this.getAddQuestion("checkBox")}
+            content="Checkbox"
+          />
+          <Button
+            icon="chevron down"
+            onClick={this.getAddQuestion("dropDown")}
+            content="Drop Down"
+          />
+        </Segment>
+        <Button
+          fluid
+          primary
+          style={{ maxWidth: "25%", margin: "auto" }}
+          size="huge"
+          content="Publish"
+        />
+      </React.Fragment>
     );
   }
 }
---
+++ client/src/components/QuestionField.tsx
@@ -0,0 +1,178 @@
+import * as React from "react";
+import {
+  Form,
+  Grid,
+  Icon,
+  Input,
+  Menu,
+  Message,
+  TextArea
+} from "semantic-ui-react";
+import { IQuestion } from "../DataTypes";
+
+interface IProps {
+  question: IQuestion;
+  setQuestion: (question: IQuestion) => void;
+  deleteQuestion: () => void;
+}
+
+interface IState {
+  option: string;
+}
+
+class QuestionField extends React.Component<IProps, IState> {
+  public constructor(props: IProps) {
+    super(props);
+    this.addOption = this.addOption.bind(this);
+    this.handleQuestionChange = this.handleQuestionChange.bind(this);
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.state = { option: "" };
+  }
+
+  public getAnswerField() {
+    switch (this.props.question.type) {
+      case "shortText":
+        return <input disabled placeholder="Short answer will go here" />;
+      case "longText":
+        return <TextArea disabled placeholder="Long answer will go here" />;
+      case "phone":
+        return <input disabled placeholder="(555) 555-5555" />;
+      case "email":
+        return <input disabled placeholder="example@ucla.edu" />;
+      case "dropDown":
+      case "radio":
+      case "checkBox":
+        if (this.props.question.options == null) {
+          const question = { ...this.props.question };
+          question.options = [];
+          this.props.setQuestion(question);
+        }
+        return this.getAddOptionArea();
+    }
+  }
+
+  public getAddOptionArea() {
+    return (
+      <React.Fragment>
+        <Menu vertical fluid style={{ maxWidth: "50%", margin: "auto" }}>
+          {this.props.question.options!.map((option, index) => (
+            <Menu.Item>
+              {option}
+              <Icon
+                style={{ cursor: "pointer" }}
+                name="delete"
+                onClick={this.getDeleteOption(index)}
+                color="red"
+              />
+            </Menu.Item>
+          ))}
+          <Menu.Item>
+            <Input
+              placeholder={"Add " + this.getQuestionType() + " option"}
+              onChange={this.handleOptionChange}
+              value={this.state.option}
+              action={{ icon: "plus", onClick: this.addOption }}
+            />
+          </Menu.Item>
+        </Menu>
+      </React.Fragment>
+    );
+  }
+
+  public getQuestionType() {
+    switch (this.props.question.type) {
+      case "email":
+        return "email";
+      case "phone":
+        return "phone";
+      case "longText":
+        return "long text";
+      case "shortText":
+        return "short text";
+      case "radio":
+        return "radio button";
+      case "checkBox":
+        return "checkbox";
+      case "dropDown":
+        return "drop down";
+    }
+  }
+
+  public handleQuestionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const prompt = event.target.value;
+    const question = { ...this.props.question, prompt };
+    this.props.setQuestion(question);
+  }
+
+  public handleOptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const option = event.target.value;
+    this.setState({ option });
+  }
+
+  public addOption() {
+    const option = this.state.option;
+    if (!option) {
+      return;
+    }
+    const question = { ...this.props.question };
+    if (question.options) {
+      question.options.push(option);
+    } else {
+      question.options = [option];
+    }
+    this.props.setQuestion(question);
+    this.setState({ option: "" });
+  }
+  public getDeleteOption(index: number) {
+    return () => {
+      const question = { ...this.props.question };
+      question.options!.splice(index, 1);
+      this.props.setQuestion(question);
+    };
+  }
+
+  public render() {
+    return (
+      <Message style={{ margin: "10px" }}>
+        <Grid columns={2}>
+          <Grid.Column width={15}>
+            <Form.Field>
+              <Input
+                placeholder={
+                  "Type " + this.getQuestionType() + " question here..."
+                }
+                onChange={this.handleQuestionChange}
+              />
+              <div style={{ minHeight: "5px" }} />
+              {this.getAnswerField()}
+            </Form.Field>
+          </Grid.Column>
+          <Grid.Column segment centered width={1}>
+            <Icon
+              style={
+                this.props.question.type === "email"
+                  ? null
+                  : { cursor: "pointer" }
+              }
+              name="x"
+              color="red"
+              size="big"
+              disabled={this.props.question.type === "email"}
+              onClick={
+                this.props.question.type === "email"
+                  ? null
+                  : this.props.deleteQuestion
+              }
+            />
+            {/* <Icon name="arrow up" color="black" size="large" />
+            <Icon name="arrow down" color="black" size="large" /> */}
+          </Grid.Column>
+        </Grid>
+      </Message>
+    );
+  }
+}
+
+export default QuestionField;
