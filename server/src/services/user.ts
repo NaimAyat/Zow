ffbf5e36f88f1b3dea280b3344e931c11174ab69
Austin Berke <austinberke@gmail.com>
2018-11-15 18:56:18 -0800

    Added service-level documentation

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -13,6 +13,18 @@ export interface IUserService {
 }
 
 class DatabaseUserService implements IUserService {
+  /**
+   * Retrieves an IUser matching provided username/password.
+   * Compares provided password with hashed password in database.
+   *
+   * @param username
+   *          name of user to retrieve
+   * @param password
+   *          password of user to retrieve
+   *
+   * @return IUser matching credentials
+   * @throws "User not found" if no matching user
+   */
   public async findUserByUsernameAndPassword(
     ctx: Context,
     username: string,
@@ -21,6 +33,16 @@ class DatabaseUserService implements IUserService {
     // TODO
     return null;
   }
+
+  /**
+   * Retrieves an IUser matching provided ID.
+   *
+   * @param id
+   *          id of user to retrieve
+   *
+   * @return IUser matching ID
+   * @throws "User not found" if no matching user
+   */
   public async findUserById(ctx: Context, id: string): Promise<IUser> {
     const user = await User.findById(id);
     if (!user) {
@@ -29,6 +51,17 @@ class DatabaseUserService implements IUserService {
 
     return user;
   }
+  /**
+   * Creates a new user with the given username and password.
+   * Hashes password with Encryption Service before entering in database.
+   *
+   * @param username
+   *          username of new user
+   * @param password
+   *          password of new user
+   *
+   * @return void
+   */
   public async newUser(
     ctx: Context,
     username: string,
