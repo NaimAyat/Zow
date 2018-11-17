953b149212038b5f6461140cc143dc6ecd73111c
Christopher Aziz <caziz@ucla.edu>
2018-11-16 20:27:06 -0800

    Add score page

--- client/src/components/ScoringPage.tsx
+++ client/src/components/ScoringPage.tsx
@@ -1,9 +1,96 @@
 import * as React from "react";
-import { Header } from "semantic-ui-react";
+import {
+  Button,
+  Header,
+  TextArea,
+  TextAreaProps,
+  Rating,
+  Form,
+  Message
+} from "semantic-ui-react";
+import { IAnswer, IResponse, IQuestion } from "../DataTypes";
+import Card from "./Card";
 
-class ScoringPage extends React.Component {
+interface IProps {
+  formName: string;
+  questions: IQuestion[];
+  response: IResponse;
+}
+
+interface IState {
+  rating: number;
+  notes: string;
+}
+
+class ScoringPage extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.handleRate = this.handleRate.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.state = {
+      rating: 0,
+      notes: ""
+    };
+  }
+  public handleRate(event: any, { rating }: any) {
+    this.setState({ rating });
+  }
+  public handleTextChange(event: React.SyntheticEvent, data: TextAreaProps) {
+    this.setState({ notes: String(data.value) });
+  }
+
+  public getAnswer(question: IQuestion, answer: IAnswer) {
+    const text = answer.answer;
+
+    if (question.type === "checkbox") {
+      const words = text.split(",");
+      return words.map((word, index) => <p key={index}>{word}</p>);
+    }
+    return <p>{text}</p>;
+  }
   public render() {
-    return <Header as="h3">Scoring Page</Header>;
+    return (
+      <React.Fragment>
+        <Card>
+          <div style={{ textAlign: "center" }}>
+            <Form size="big">
+              <Header textAlign="left" as="h1" content="Score Applicant" />
+              <Form.Field>
+                <Rating
+                  clearable
+                  icon="star"
+                  maxRating={5}
+                  style={{ fontSize: "4rem" }}
+                  onRate={this.handleRate}
+                />
+              </Form.Field>
+              <Form.Field>
+                <TextArea
+                  placeholder="Add notes"
+                  onChange={this.handleTextChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Button primary size="huge" content="Score" />
+              </Form.Field>
+            </Form>
+          </div>
+        </Card>
+        <Card>
+          <Header as="h1">{this.props.formName}</Header>
+          <Form size="big">
+            {this.props.questions.map((question, index) => (
+              <Message style={{ margin: "10px" }}>
+                <Form.Field required>
+                  <label>{question.prompt}</label>
+                  {this.getAnswer(question, this.props.response.answers[index])}
+                </Form.Field>
+              </Message>
+            ))}
+          </Form>
+        </Card>
+      </React.Fragment>
+    );
   }
 }
 
